08dcee40175d9f44051e6abe47155f8f14310039
yarenty <yarenty@gmail.com>
2015-03-12 13:04:51 +0000

    Cufft + Currand + math FFT stuff

---
+++ src/main/java/com/yarenty/cufft/JCufftSample.java
@@ -0,0 +1,94 @@
+package com.yarenty.cufft;
+
+/*
+ * JCufft - Java bindings for CUFFT, the NVIDIA CUDA FFT library,
+ * to be used with JCuda<br />
+ * http://www.jcuda.org
+ *
+ * Copyright 2009 Marco Hutter - http://www.jcuda.org
+ */
+
+import edu.emory.mathcs.jtransforms.fft.FloatFFT_1D;
+import jcuda.jcufft.JCufft;
+import jcuda.jcufft.cufftHandle;
+import jcuda.jcufft.cufftType;
+
+import java.util.Random;
+
+/**
+ * This is a sample class that performs a 1D Complex-To-Complex
+ * forward FFT with JCufft, and compares the result to the
+ * reference computed with JTransforms.
+ */
+class JCufftSample
+{
+    public static void main(String args[])
+    {
+        testC2C1D(1<<20);
+    }
+
+    /**
+     * Test the 1D C2C transform with the given size.
+     *
+     * @param size The size of the transform
+     */
+    private static void testC2C1D(int size)
+    {
+        System.out.println("Creating input data...");
+        float input[] = createRandomFloatData(size * 2);
+
+        System.out.println("Performing 1D C2C transform with JTransforms...");
+        float outputJTransforms[] = input.clone();
+        FloatFFT_1D fft = new FloatFFT_1D(size);
+        fft.complexForward(outputJTransforms);
+
+        System.out.println("Performing 1D C2C transform with JCufft...");
+        float outputJCufft[] = input.clone();
+        cufftHandle plan = new cufftHandle();
+        JCufft.cufftPlan1d(plan, size, cufftType.CUFFT_C2C, 1);
+        JCufft.cufftExecC2C(plan, outputJCufft, outputJCufft, JCufft.CUFFT_FORWARD);
+        JCufft.cufftDestroy(plan);
+
+        boolean passed = isCorrectResult(outputJTransforms, outputJCufft);
+        System.out.println("testC2C1D "+(passed?"PASSED":"FAILED"));
+    }
+
+    /**
+     * Creates an array of the specified size, containing some random data
+     */
+    private static float[] createRandomFloatData(int x)
+    {
+        Random random = new Random(1);
+        float a[] = new float[x];
+        for (int i=0; i<x; i++)
+        {
+            a[i] = random.nextFloat();
+        }
+        return a;
+    }
+
+    /**
+     * Compares the given result against a reference, and returns whether the
+     * error norm is below a small epsilon threshold
+     */
+    private static boolean isCorrectResult(float result[], float reference[])
+    {
+        float errorNorm = 0;
+        float refNorm = 0;
+        for (int i = 0; i < result.length; ++i)
+        {
+            float diff = reference[i] - result[i];
+            errorNorm += diff * diff;
+            refNorm += reference[i] * result[i];
+        }
+        errorNorm = (float) Math.sqrt(errorNorm);
+        refNorm = (float) Math.sqrt(refNorm);
+        if (Math.abs(refNorm) < 1e-6)
+        {
+            return false;
+        }
+        return (errorNorm / refNorm < 1e-6f);
+    }
+
+
+}
---
+++ src/main/java/com/yarenty/curand/JCurandSample.java
@@ -0,0 +1,64 @@
+package com.yarenty.curand;
+
+/*
+ * JCurand - Java bindings for CURAND, the NVIDIA CUDA random
+ * number generation library, to be used with JCuda
+ *
+ * Copyright (c) 2010 Marco Hutter - http://www.jcuda.org
+ */
+
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.jcurand.JCurand;
+import jcuda.jcurand.curandGenerator;
+import jcuda.runtime.JCuda;
+
+import java.util.Arrays;
+
+import static jcuda.jcurand.JCurand.*;
+import static jcuda.jcurand.curandRngType.CURAND_RNG_PSEUDO_DEFAULT;
+import static jcuda.runtime.JCuda.*;
+import static jcuda.runtime.cudaMemcpyKind.cudaMemcpyDeviceToHost;
+
+/**
+ * A small sample application showing how to use JCurand.<br />
+ * <br />
+ * Basically ported from the NVIDIA CURAND documentation example.
+ */
+public class JCurandSample {
+    public static void main(String args[]) {
+        // Enable exceptions and omit all subsequent error checks
+        JCuda.setExceptionsEnabled(true);
+        JCurand.setExceptionsEnabled(true);
+
+        int n = 100;
+        curandGenerator generator = new curandGenerator();
+
+        // Allocate n floats on host
+        float hostData[] = new float[n];
+
+        // Allocate n floats on device
+        Pointer deviceData = new Pointer();
+        cudaMalloc(deviceData, n * Sizeof.FLOAT);
+
+        // Create pseudo-random number generator
+        curandCreateGenerator(generator, CURAND_RNG_PSEUDO_DEFAULT);
+
+        // Set seed
+        curandSetPseudoRandomGeneratorSeed(generator, 1234);
+
+        // Generate n floats on device
+        curandGenerateUniform(generator, deviceData, n);
+
+        // Copy device memory to host
+        cudaMemcpy(Pointer.to(hostData), deviceData,
+                n * Sizeof.FLOAT, cudaMemcpyDeviceToHost);
+
+        // Show result
+        System.out.println(Arrays.toString(hostData));
+
+        // Cleanup
+        curandDestroyGenerator(generator);
+        cudaFree(deviceData);
+    }
+}
